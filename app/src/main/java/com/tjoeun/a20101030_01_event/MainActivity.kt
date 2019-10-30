0325bd2aaea74c696dd5b96c36de6b7abf64469f
sjsid79 <wjdtlr0723>
2019-10-30 20:02:23 +0900

    액티비티 실행시 텍스트뷰 글씨 변경

--- app/src/main/java/com/tjoeun/a20101030_01_event/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20101030_01_event/MainActivity.kt
@@ -2,11 +2,16 @@ package com.tjoeun.a20101030_01_event
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // xml에 그려진 초기 값 이라고 적힌 텍스트뷰의 글자를 "실행 후"로 변경
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firstTextView.text = "실행 후"
     }
 }
